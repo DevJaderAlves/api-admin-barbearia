beba55d3facadf3d799c2f01e7635e161ade51c2
Jader Alves <DevJaderAlves@gmail.com>
2025-05-05 19:46:10 -0300

    Atualizando o connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,21 @@
+// const mysql = require('mysql2/promise');
+// require('dotenv').config();
+
+// const connection = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+// });
+
+// module.exports = connection;
+
+
+
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-});
+const connection = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = connection;
